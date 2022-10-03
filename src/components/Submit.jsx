16048c237c046cfc5b87328f98e289b386f0c810
vanityadav <vanityadav08@gmail.com>
2022-10-03 23:07:04 +0530

    Update Submit.jsx

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function Submit() {
   return (
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="post" netlify>
       <input type="hidden" name="form-name" value="contact" />
-
       <p>
         <label>
           Your Name: <input type="text" name="name" />
@@ -13,15 +12,6 @@ export default function Submit() {
       <p>
         <label>
           Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Role:{" "}
-          <select name="role[]" multiple>
-            <option value="leader">Leader</option>
-            <option value="follower">Follower</option>
-          </select>
         </label>
       </p>
       <p>
