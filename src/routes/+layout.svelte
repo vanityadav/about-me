591b68dd8afcd453322f22c2e901bb4143bf8ea0
Vanit Yadav <110538930+vanityadav@users.noreply.github.com>
2023-06-25 01:21:50 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,10 @@
 		content="Vanit Yadav, oneIT, developer, webd, Web Developer, CSS, JavaScript, Svelte , SvelteKit, React, Software Developer, Software engineer , Html ,Ijdro Developer"
 	/>
 	<meta name="author" content="Vanit Yadav" />
+	<meta
+		name="description"
+		content="Welcome to the professional portfolio of Vanit Yadav, a skilled front-end developer and team leader. With expertise in React, Next.js, Sevelt, Tailwind CSS and more. I create visually stunning and responsive web applications. Explore my work and projects to see how I bring innovative designs to life. Contact me to collaborate on your next web development venture."
+	/>
 </svelte:head>
 <Header />
 <main>
