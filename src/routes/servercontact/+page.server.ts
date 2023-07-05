db011e25c8466c1a110b2a839e4874e1dcfd21fa
Vanit Yadav <110538930+vanityadav@users.noreply.github.com>
2023-07-05 16:57:04 +0530

    fixes

--- src/routes/servercontact/+page.server.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('./$types').PageServerLoad} */
-import prisma from '$lib/server/prisma/PrismaClient';
-
-export async function load() {
-	const res = await prisma.contact.findMany();
-	return { data: res };
-}
