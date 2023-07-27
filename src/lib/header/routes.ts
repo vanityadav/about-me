13f9657d6e6cc731cf913fd8ba6edf8409431a7c
Vanit Yadav <110538930+vanityadav@users.noreply.github.com>
2023-07-27 16:36:12 +0530

    fixes

--- src/lib/header/routes.ts
+++ src/lib/header/routes.ts
@@ -1,11 +1,14 @@
 import messenger from '$lib/assets/dock/messenger.svg';
+import mail from '$lib/assets/dock/mail.svg';
+import resume from '$lib/assets/dock/resume.svg';
 
 const dockRoutes = [
 	{ href: '', src: messenger, label: 'Home' },
 	{ href: 'about', src: messenger, label: 'About' },
 	{ href: 'projects', src: messenger, label: 'Project' },
 	{ href: 'contact', src: messenger, label: 'Contact' },
-	{ href: 'resume', src: messenger, label: 'Resume' }
+	{ href: 'resume', src: resume, label: 'Resume' },
+	{ href: 'mailto:vanityadav08@gmail.com', src: mail, label: 'Mail me' }
 ];
 
 export default dockRoutes;
